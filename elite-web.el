31f84b66b5f414fc84a81bf71f1672139dafb577
YongjieZhao <yongjie.zhao@wz-inc>
2016-09-17 21:38:19 +0800

    update js2-mode2

--- elite-web.el
+++ elite-web.el
@@ -2,7 +2,6 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js?\\'" . js2-mode))
 
 (defun custom-web-mode-hook ()
   "Hooks for Web mode."
@@ -17,21 +16,15 @@
 )
 (add-hook 'web-mode-hook 'custom-web-mode-hook)
 
+;; setting js2-mode
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
 
-;; setting js2
-(custom-set-variables
- '(js2-basic-offset 2)
- '(js2-global-externs (list "$" "window" "angular"))
+(defun custom-js2-mode-hook ()
+  (setq js2-basic-offset 2)
+  (setq js2-global-externs '("$" "window" "angular"))
+  ;; (flycheck-mode -1) ;; 关闭flycheck-mode
 )
-(add-hook 'js-mode-hook 'js2-minor-mode)
-(defun custom-js-mode-hook()
-  (setq flycheck-mode 1)
-  ;; (setq jslint-mode -1)
-)
-(add-hook 'js-mode-hook 'custom-js-mode-hook)
-;; (add-hook 'js-mode-hook 'flycheck-mode) ;; 打开flycheck-mode
-;; (add-hook 'js-mode-hook 'jslint-mode)
-;; (add-hook 'js-mode-hook (lambda () (flycheck-mode -1))) ;; 关闭flycheck-mode
-;; (add-hook 'js-mode-hook (lambda () (jslint-mode t))) ;; 关闭flycheck-mode
+(add-hook 'js2-mode-hook 'custom-js2-mode-hook)
 
 (provide 'elite-web)
